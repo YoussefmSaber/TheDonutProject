1863ba20d21549904bb70fe0dd61c4f579bcb113
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-06-02 19:35:14 +0300

    Added new Colors and Dimensions

--- app/src/main/java/com/youssefmsaber/thedonutproject/ui/theme/Color.kt
+++ app/src/main/java/com/youssefmsaber/thedonutproject/ui/theme/Color.kt
@@ -12,8 +12,11 @@ val Pink40 = Color(0xFF7D5260)
 
 val PrimaryPink = Color(0xFFFF7074)
 val SecondaryPink = Color(0xFFFF9494)
+val LittleLightPink = Color(0xFFFFC7D0)
 val LightPink = Color(0xFFFED8DF)
 val White = Color(0xFFFFFFFF)
 
 val Black = Color(0xFF000000)
 val Gray = Color(0xFF585858)
+
+val LightBlue = Color(0xFFD7E4F6)
---
+++ app/src/main/java/com/youssefmsaber/thedonutproject/ui/theme/Dimens.kt
@@ -0,0 +1,41 @@
+package com.youssefmsaber.thedonutproject.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val Padding4 = 4.dp
+val Padding8 = 8.dp
+val Padding12 = 12.dp
+val Padding15 = 15.dp
+val Padding16 = 16.dp
+val Padding20 = 20.dp
+val Padding24 = 24.dp
+
+val Radius4 = 4.dp
+val Radius8 = 8.dp
+val Radius10 = 10.dp
+val Radius12 = 12.dp
+val Radius15 = 15.dp
+val Radius16 = 16.dp
+val Radius20 = 20.dp
+val Radius24 = 24.dp
+val Radius32 = 32.dp
+
+val IconSize16 = 16.dp
+val IconSize24 = 24.dp
+val IconSize32 = 32.dp
+val IconSize48 = 48.dp
+val IconSize64 = 64.dp
+
+val NegativePadding4 = (-4).dp
+val NegativePadding8 = (-8).dp
+val NegativePadding12 = (-12).dp
+val NegativePadding16 = (-16).dp
+val NegativePadding24 = (-24).dp
+
+
+val Elevation0 = 0.dp
+val Elevation4 = 4.dp
+val Elevation8 = 8.dp
+val Elevation12 = 12.dp
+val Elevation16 = 16.dp
+val Elevation24 = 24.dp
