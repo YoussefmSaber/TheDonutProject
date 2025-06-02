39269b76eaf7a084eaeb9b5aeb901be77e5f5b17
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-06-02 13:36:39 +0300

    Initial commit

---
+++ app/src/main/java/com/youssefmsaber/thedonutproject/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.youssefmsaber.thedonutproject.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.youssefmsaber.thedonutproject.R
+
+val INTER = FontFamily(
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_semi_bold, FontWeight.SemiBold),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_regular, FontWeight.Normal),
+)
